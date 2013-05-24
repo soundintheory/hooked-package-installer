6c524c8853e8a813c2ddc87338c0b1e8fc7271b3
Andy White <andy@soundintheory.co.uk>
2013-05-24 16:59:07 +0100

    Started to add hook support

--- src/CMF/Composer/HookedPackageInstaller.php
+++ src/CMF/Composer/HookedPackageInstaller.php
@@ -21,7 +21,38 @@ class HookedPackageInstaller extends \Composer\Installer\LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        return parent::getInstallPath($package);
+        $type = $package->getType();
+
+        $prettyName = $package->getPrettyName();
+        if (strpos($prettyName, '/') !== false) {
+            list($vendor, $name) = explode('/', $prettyName);
+        } else {
+            $vendor = '';
+            $name = $prettyName;
+        }
+
+        $availableVars = compact('name', 'vendor', 'type');
+
+        $extra = $package->getExtra();
+        if (!empty($extra['installer-name'])) {
+            $availableVars['name'] = $extra['installer-name'];
+        }
+
+        if ($this->composer->getPackage()) {
+            $pkg_extra = $this->composer->getPackage()->getExtra();
+            if (!empty($pkg_extra['installer-paths'])) {
+                $customPath = $this->mapCustomInstallPaths($pkg_extra['installer-paths'], $prettyName, $type);
+                if ($customPath !== false) {
+                    return $this->templatePath($customPath, $availableVars);
+                }
+            }
+        }
+        
+        if (empty($extra['installer-path'])) {
+            return parent::getInstallPath($package);
+        }
+
+        return $this->templatePath($extra['installer-path'], $availableVars);
     }
     
     /**
@@ -29,9 +60,9 @@ class HookedPackageInstaller extends \Composer\Installer\LibraryInstaller
      */
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        print("\nCMF PRE INSTALL\n");
+        $this->callHook('pre-package-install', $package);
         parent::install($repo, $package);
-        print("\nCMF POST INSTALL\n");
+        $this->callHook('post-package-install', $package);
     }
 
     /**
@@ -39,9 +70,9 @@ class HookedPackageInstaller extends \Composer\Installer\LibraryInstaller
      */
     public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
     {
-        print("\nCMF PRE UPDATE\n");
+        $this->callHook('pre-package-update', $package);
         parent::update($repo, $initial, $target);
-        print("\nCMF POST UPDATE\n");
+        $this->callHook('post-package-update', $package);
     }
 
     /**
@@ -49,9 +80,54 @@ class HookedPackageInstaller extends \Composer\Installer\LibraryInstaller
      */
     public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
-        print("\nCMF PRE UNINSTALL\n");
+        $this->callHook('pre-package-uninstall', $package);
         parent::uninstall($repo, $package);
-        print("\nCMF POST UNINSTALL\n");
+        $this->callHook('post-package-uninstall', $package);
+    }
+    
+    /**
+     * Attempts to call a hook
+     * 
+     * @param  string           $hookName
+     * @param  PackageInterface $package
+     */
+    protected function callHook($hookName, PackageInterface $package)
+    {
+        // $composer, $package
+        $extra = $package->getExtra();
+        if (empty($extra[$hookName])) return;
+        
+        $command = $extra[$hookName];
+        $callable = is_callable($command);
+        if (strpos($command, '::') !== false) {
+            $parts = explode('::', $command);
+            $class = $parts[0];
+            $exists = class_exists($class);
+            print("\n".$hookName.": ".$class." ".($exists ? "does exist" : "doesn't exist")."\n");
+        }
+        print("\n".$hookName.": ".$command." is ".($callable ? "callable" : "not callable")."\n");
+    }
+
+    /**
+     * Replace vars in a path
+     *
+     * @param  string $path
+     * @param  array  $vars
+     * @return string
+     */
+    protected function templatePath($path, array $vars = array())
+    {
+        if (strpos($path, '{') !== false) {
+            extract($vars);
+            preg_match_all('@\{\$([A-Za-z0-9_]*)\}@i', $path, $matches);
+            if (!empty($matches[1])) {
+                foreach ($matches[1] as $var) {
+                    $path = str_replace('{$' . $var . '}', $$var, $path);
+                }
+            }
+        }
+
+        return $path;
     }
 
     /**
